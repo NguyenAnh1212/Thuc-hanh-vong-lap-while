fa1fb3d469ff943c5667a9efb230e6f0f7fbf24f
unknown <anhnt.tb12@gmail.com>
2022-01-10 23:10:45 +0700

    bai2

--- vonglapwhile.js
+++ vonglapwhile.js
@@ -1,1 +1,17 @@
+let num = prompt("Enter a number: ");
+let total = 0;
+while( num != -1 ) {
+    num = Number(prompt("Enter a number: "));
+    alert(num);
+    total += num;
+    alert(total)
+   }
+
+let i = 1;
+
+while (i < 100) {
+
+    document.write("<hr width = " + i + "%>");
+    i++;
+}
 
